b19f3a5ace8b6a16ba745f94c325613efd589641
Jendnice <Pipeline38@hotmail.com>
2020-02-07 17:21:31 -0500

    Done.

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react'
+import BandInput from '../components/BandInput'
+import { connect } from 'react-redux'
 
 class BandsContainer extends Component {
   render() {
     return(
       <div>
         BandsContainer
+        <BandInput 
+        addBand={this.props.addBand}
+        />
+
+        {this.props.bands.map(band => { return (
+          <li key={band.name}>
+            {band.name}
+          </li>)
+        })}
       </div>
     )
   }
 }
 
-export default BandsContainer
+const mapStateToProps = (state) => {
+  return {
+    bands: state.bands
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addBand: (formData) => dispatch({type: 'ADD_BAND', payload: formData})
+  }
+}
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     addTodo: formData => dispatch({type: 'ADD_TODO', payload: formData})
+//   }
+// }
+
+
+export default connect (mapStateToProps, mapDispatchToProps)(BandsContainer)
+
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,5 +1,17 @@
 export default function manageBand(state = {
   bands: [],
 }, action) {
-  return state
+
+  // debugger
+
+  switch (action.type) {
+    case 'ADD_BAND':
+      // return { todos: state.todos.concat(action.payload.text) };
+      return { bands: state.bands.concat(action.payload) }
+
+    default: 
+      return state 
+  }
+
 };
+
